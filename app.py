50a1dc72215c6c06cb8761ad261aa014d4b8098c
KyawNgwe <98771981+KyawNgwe@users.noreply.github.com>
2026-08-16 01:44:16 +0700

    创建 app.py

---
+++ app.py
@@ -0,0 +1,4 @@
+# ===== 配置区域（请修改为你自己的信息）=====
+TELEGRAM_BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # 替换为你的 Bot Token
+TELEGRAM_CHAT_ID = "123456789"      # 替换为你的 Chat ID（群组ID记得带负号）
+# =========================================
