290d572ae29a908c4f951fcc9a1a098b6235abc6
KyawNgwe <98771981+KyawNgwe@users.noreply.github.com>
2026-08-16 01:49:04 +0700

    删除 app.py

--- app.py
+++
@@ -1,4 +0,0 @@
-# ===== 配置区域（请修改为你自己的信息）=====
-TELEGRAM_BOT_TOKEN = 8939398684:AAHscEJ4st2XHCBNgFpcfpFH7Cu8MqNLTjk"  # 替换为你的 Bot Token
-TELEGRAM_CHAT_ID = "1949334561"      # 替换为你的 Chat ID（群组ID记得带负号）
-# =========================================
